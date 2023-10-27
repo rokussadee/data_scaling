a8096f0ffb719841fb4c83a6fe695213f5464ee7
rokussadee <rokus.sadee@student.ehb.be>
2023-10-27 07:47:52 +0200

    build(rust-actix): rust + actix setup
    - data_scaling/src/main.rs: main file with basic routes
    - .gitignore
    - Cargo.toml

---
+++ data_scaling/src/main.rs
@@ -0,0 +1,26 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::io;
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(index)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
